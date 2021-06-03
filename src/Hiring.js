d050ced023aa117f04f89e2a9579205fc00848e1
Chris Evans <chris.evans1@gmail.com>
2021-06-03 21:36:09 +0200

    Completed work on hiring page

--- src/Hiring.js
+++ src/Hiring.js
@@ -9,26 +9,28 @@ let params = new URLSearchParams(search);
 let email = params.get('email');
 
 
-const handleLogin = async googleData => {
 
-    const json = JSON.stringify({ 
-      email: email,
-      work_email: googleData.profileObj.email,
-      creds : googleData.qc});
-
-    const response = await axios
-      .post(`http://localhost:3000/work-connection/work-email`,
-        json,{
-          headers: { 
-            'Content-Type': 'application/json'
-          }
-        }).then( async data => {
-         // console.log(JSON.stringify(data));
-        });
-      }
 
 
 const Hiring = () => {
+    
+    const handleLogin = async googleData => {
+
+        const json = JSON.stringify({ 
+          email: email,
+          work_email: googleData.profileObj.email,
+          creds : googleData.qc});
+    
+        const response = await axios
+          .post(`http://localhost:3000/work-connection/work-email`,
+            json,{
+              headers: { 
+                'Content-Type': 'application/json'
+              }
+            }).then( async data => {
+                setFinished(true);
+            });
+          }
 
     const [finished, setFinished] = useState(false);
 
@@ -52,6 +54,11 @@ const Hiring = () => {
         <div id='divCentred'>
 
             <h1>Product Design Role: Workdock</h1>
+
+            {finished && <div>
+            <p>Thanks, we will contact you shortly about the role and to get feedback on using this</p> </div>}
+
+            {finished === false && <div>
             <p>Connect your WORK email so we can check you meeting history and see if we have any common connections.  
                 By doing this, the more connections we have in common the more your application will go to the top of the list. </p>
     
@@ -68,6 +75,8 @@ const Hiring = () => {
         />
 
         <p id='ps'>p.s. We don't use this data for anything else, and we won't contact anyone from your list until we ask you if it is ok.</p>
+        </div>
+            }
       </div>
       );
 }
